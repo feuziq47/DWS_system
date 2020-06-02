87ac7746a91a8514f51956c4627cce6c482c0b94
feuziq47 <feuziq47@gmail.com>
2020-06-02 19:50:06 +0900

    STW implementation

---
+++ src/main/java/Model/StopwatchMode.java
@@ -0,0 +1,77 @@
+package Model;
+import java.util.*;
+
+/**
+ * 
+ */
+public class StopwatchMode {
+
+    /**
+     * Default constructor
+     */
+    public StopwatchMode() {
+    }
+
+    /**
+     * 
+     */
+    private boolean OnOff;
+
+    /**
+     * 
+     */
+    private Stopwatchtime stopwatchTime;
+
+    /**
+     * 
+     */
+    private Stopwatchtime laptime;
+
+
+
+
+    /**
+     * @param currentState
+     */
+    public void changeMode(int currentState) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public Stopwatchtime calStopwatchTime() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * 
+     */
+    public void startStopwatch() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void stopStopwatch() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void resetStopwatch() {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public Stopwatchtime lapTime() {
+        // TODO implement here
+        return null;
+    }
+
+}
