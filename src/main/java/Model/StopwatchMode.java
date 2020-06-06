857f5b03cc4eeac9a1eda0be25fcc27c21c3083b
도연 김 <dy857@naver.com>
2020-06-06 18:25:36 +0900

    stw

--- src/main/java/Model/StopwatchMode.java
+++ src/main/java/Model/StopwatchMode.java
@@ -1,32 +1,59 @@
 package Model;
 import java.util.*;
-
+import java.util.TimerTask;
+import java.util.Timer;
 /**
  * 
  */
 public class StopwatchMode {
 
+    private int min;
+    private int sec;
+    private int msec;
+    private int lapTime;
+    private boolean OnOff;
+
+    Timer timer = new Timer();
     /**
      * Default constructor
      */
     public StopwatchMode() {
+        this.OnOff = false;
+        this.min = 0;
+        this.sec = 0;
+        this.msec = 0;
+        this.lapTime = 0;
     }
 
-    /**
-     * 
-     */
-    private boolean OnOff;
+    TimerTask task = new TimerTask() {
 
-    /**
-     * 
-     */
-    private Stopwatchtime stopwatchTime;
+        public void run() {
+            while(true) {
+                if(OnOff == true) {
+                    msec+=1;
+                    if (msec == 100) {
+                        msec = 0;
+                        sec += 1;
+                    }
+                    if (sec == 60) {
+                        sec = 0;
+                        min += 1;
+                    }//min이 59를 넘으면 어떻게 될지... 예외상황 있음
+                    try {
+                        Thread.sleep(9);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    System.out.println(min+" : "+sec+ " : " + msec);
+                }
+                else {
+                    System.out.println(min+" : "+sec+ " : " + msec);
+                }
+            }
 
-    /**
-     * 
-     */
-    private Stopwatchtime laptime;
-
+        }
+    };
 
 
 
@@ -49,21 +76,27 @@ public class StopwatchMode {
      * 
      */
     public void startStopwatch() {
-        // TODO implement here
+        this.OnOff = true;
+        timer = new Timer();
+        timer.scheduleAtFixedRate(task, 0, 1);
     }
 
     /**
      * 
      */
     public void stopStopwatch() {
-        // TODO implement here
+        this.OnOff = false;
     }
 
     /**
      * 
      */
     public void resetStopwatch() {
-        // TODO implement here
+        this.OnOff = false;
+        timer = null;
+        this.msec = 0;
+        this.sec = 0;
+        this.min = 0;
     }
 
     /**
